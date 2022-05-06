ad50ea7c5ac54088c57500e1a0864c719701b3c4
ImTobacho <105048539+ImTobacho@users.noreply.github.com>
2022-05-06 12:22:54 +0100

    Update js.py
    FUNCIONAAAAAAAAAAAAAAA

--- js.py
+++ js.py
@@ -1,18 +1,19 @@
 import csv
 import json
-import json
+
+
 
 with open('result.py', 'r') as file:
-    data = file.read()
-info = json.loads(data)
+    data = file.read().replace('\n', '')
+    
+    info = json.loads(data)
+    fieldnames = info.keys()
+    
 
-print(info[0].keys())
+    print(info.keys())
 
-with open("samplecsv.csv", 'w') as f: 
-    reader = csv.reader(f)
-    wr = csv.DictWriter(f, fieldnames = info[0].keys()) 
-    wr.writeheader() 
-    wr.writerows(info)
-
-    fechas=data.read().split('||')
-    print(fechas)
+    with open("samplecsv.csv", 'w') as f: 
+            reader = csv.reader(f)
+            wr = csv.DictWriter(f, fieldnames = info.keys()) 
+            wr.writeheader() 
+            wr.writerows(info.keys)
